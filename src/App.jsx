1ccc2a993680133182c1a294f657f940943766dc
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-25 02:17:15 +0530

    react component build

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Landing from './page/Landing'
+
+function App() {
+
+  return (
+    <div className="bg-[#0a0c1b]">
+      <Landing />
+    </div>
+  );
+}
+
+export default App
---
+++ src/page/Landing.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#0a0a16] px-6">
+      <div className="flex flex-col-reverse md:flex-row bg-[#1c1938] rounded-lg overflow-hidden w-full max-w-4xl md:max-w-6xl">
+        {/* Left Side */}
+        <div className="p-16 flex flex-col justify-center text-white md:w-2/3 text-center md:text-left">
+          <h1 className="text-4xl font-semibold">
+            Get <span className="text-[#9b63c5]">insights</span> that help{" "}
+            <br className="hidden md:block" />
+            your business grow.
+          </h1>
+          <p className="opacity-70 mt-10 md:text-sm text-lg leading-relaxed">
+            Discover the benefits of data analytics and make{" "}
+            <br className="hidden md:block" />
+            better decisions regarding revenue, customer{" "}
+            <br className="hidden md:block" />
+            experience, and overall efficiency.
+          </p>
+          <div className="flex flex-col md:flex-row justify-between mt-16 space-y-5 md:space-y-0">
+            <div className="text-center md:text-left">
+              <span className="md:text-2xl text-3xl font-bold">10k+</span>
+              <p className="opacity-70 text-xs">COMPANIES</p>
+            </div>
+            <div className="text-center md:text-left">
+              <span className="md:text-2xl text-3xl font-bold">314</span>
+              <p className="opacity-70 text-xs">TEMPLATES</p>
+            </div>
+            <div className="text-center md:text-left">
+              <span className="md:text-2xl text-3xl font-bold">12M+</span>
+              <p className="opacity-70 text-xs">QUERIES</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Side - Image with Overlay */}
+        <div className="md:w-2/3 relative">
+          <img
+            src="/src/images/image-header-desktop.jpg"
+            alt="Analytics team"
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-purple-600 mix-blend-multiply opacity-60"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
