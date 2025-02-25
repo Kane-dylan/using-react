413c609ba8ff87a1ef46b0148537a9dd05bf1768
Kane Dylan <cool.kdtech1234@gmail.com>
2025-02-25 23:42:56 +0530

    cooking added

---
+++ src/page/Cooking.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const Cooking = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#f3e5d8]">
+      <div className="max-w-4xl sm:p-10 md:m-44 bg-white sm:rounded-3xl shadow-lg">
+        <div className="omelette bg-cover relative w-full h-[40vh] sm:rounded-2xl"></div>
+        <h1 className="text-4xl young-serif-regular mt-8">
+          Simple Omelette Recipe
+        </h1>
+        <p className="outfit text-[#9f9e9a] mt-4">
+          An easy and quick dish, perfect for any meal. This classic omelette
+          combines beaten eggs cooked to perfection, optionally filled with your
+          choice of cheese. vegetables, or meats.
+        </p>
+        <div className="bg-[#fff7fc] outfit p-6 rounded-lg mt-8">
+          <h2 className="text-xl mb-2 font-extrabold text-[#6c334b]">
+            Preparation time
+          </h2>
+          <ol className="flex flex-col gap-2 text-[#726864]">
+            <li className="font-medium">
+              •<span className="font-bold ml-5">Total</span>: Approximately 10
+              minutes
+            </li>
+            <li className="font-medium">
+              •<span className="font-bold ml-5">Preparation</span>: 5 minutes
+            </li>
+            <li className="font-medium">
+              •<span className="font-bold ml-5">Cooking</span>: 5 minutes
+            </li>
+          </ol>
+        </div>
+        <div className="mt-8">
+          <h2 className="young-serif-regular text-[#78453a] text-3xl font-medium">
+            Ingredients
+          </h2>
+          <ul>
+            <li>• 2-3 large eggs</li>
+            <li>• Salt, to taste</li>
+            <li>• Pepper, to taste</li>
+            <li>• 1 tablespoon of butter or oil</li>
+            <li>
+              • Optional fillings: cheese,diced vegetables, cooked meats, herbs
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ol>
+            <li>
+              <span>Beat the eggs</span>: In a bowl, beat the eggs with a pinch
+              of salt and pepper until they are well mixed. You can add a
+              tablespoon of water or milk for a fluffier texture.
+            </li>
+            <li>
+              <span>Heat the pan</span>: Place a non-stick frying pan over
+              medium heat and add butter or oil.
+            </li>
+            <li>
+              <span>Cook the omelette</span>: Once the butter is melted and
+              bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly
+              coat the surface.
+            </li>
+            <li>
+              <span>Add fillings (optional)</span>: When the eggs begin to set
+              at the edges but are still slightly runny in the middle, sprinkle
+              your chosen fillings over one half of the omelette.
+            </li>
+            <li>
+              <span>Fold and serve</span>: As the omelette continues to cook,
+              carefully lift one edge and fold it over the fillings. Let it cook
+              for another minute, then slide it onto a plate.
+            </li>
+            <li>
+              <span>Enjoy</span>: Serve hot, With additional salt and pepper if
+              needed.
+            </li>
+          </ol>
+        </div>
+        <div>
+          <h2>Nutrition</h2>
+          <p>
+            The table below shows nutritional values per serving without the
+            additional fillings.
+          </p>
+          <table>
+            <thead>
+              <tr>
+                <th>Calories</th>
+                <th>Protein</th>
+                <th>Fat</th>
+                <th>Carbs</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>210 kcal</td>
+                <td>13g</td>
+                <td>16g</td>
+                <td>1g</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cooking
